58681c49b274f660f39171fd59ca9bef6c52ebcd
FelixJacques <felix.jacques@infobec.ca>
2021-04-15 18:26:35 -0400

    Exemple class invest

---
+++ src/Roles/town/invest.js
@@ -0,0 +1,14 @@
+class Invest {
+    constructor(){
+        this.name = "Investigator"
+        this.description = "Vous êtes un enquêteur privé qui collecte de l’information en secret."
+        this.isUnique = false
+        this.needsTwoTargets = false
+        this.alignement = "Town investigative"
+        this.wikiLink = "https://town-of-salem.fandom.com/wiki/Investigator"
+    }
+
+    action(author, target){
+        return
+    }
+}
